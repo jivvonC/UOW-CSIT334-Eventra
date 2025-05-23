1d6375e1c291eae01486eafdeeb2b1da4192e6d0
ian <161527939+igsc25@users.noreply.github.com>
2025-05-23 13:18:09 +1000

    data updates

---
+++ frontend/my-react-app/src/pages/data/categories.js
@@ -0,0 +1,25 @@
+  const categories = [
+    { name: "All Services", icon: "/icons/all.png" },
+    { name: "Make-Up Artist", icon: "/icons/makeUpArtist.png" },
+    { name: "Hair Stylist", icon: "/icons/hairStylist.png" },
+    { name: "Photographer", icon: "/icons/photographer.png" },
+    { name: "Event Host", icon: "/icons/hosts.png" },
+    { name: "Musician", icon: "/icons/singer.png" },
+    { name: "Venue Organiser", icon: "/icons/venueOrganiser.png" },
+    { name: "Florist", icon: "/icons/florist.png" },
+    { name: "Catering", icon: "/icons/catering.png" },
+    { name: "Venue Rental", icon: "/icons/venuerental.png" },
+    { name: "Bartender", icon: "/icons/bartender.png" },
+    { name: "Mobile Bar", icon: "/icons/mobilebar.png" },
+    { name: "Barista", icon: "/icons/barista.png" },
+    { name: "Coffee", icon: "/icons/coffee.png" },
+    { name: "Ice Cream", icon: "/icons/icecreamVan.png" },
+    { name: "Carpenter", icon: "/icons/carpenter.png" },
+    { name: "Painter", icon: "/icons/painter.png" },
+    { name: "Removalist", icon: "/icons/removalist.png" },
+    { name: "Plumber", icon: "/icons/plumber.png" },
+    { name: "Cleaner", icon: "/icons/cleaning.png" },
+  ];
+
+
+  export default categories;
